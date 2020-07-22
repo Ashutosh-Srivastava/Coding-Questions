93ca2c8ba9decbc229932c5f16bca83427b6cf78
Ashutosh Srivastava <20560757+Ashutosh-Srivastava@users.noreply.github.com>
2020-07-22 17:37:53 +0530

    initial commit add solution.py

---
+++ Fuel Injection Perfection/solution.py
@@ -0,0 +1,20 @@
+# This was my solution submitted and passed all the test cases
+'''
+Author: Ashutosh Srivastava
+Python3 solution
+'''
+
+def solution(n:int)-> int:
+    table = {1: 0, 2: 1}
+    def fuel(n):
+        if n in table:
+            return table[n]
+        if n % 2 != 0:
+            #agar odd hai to +1 ya -1 karo aur minimum nikalo lekin steps 2 hi add honge so this way
+            table[n] = min(fuel((n + 1) / 2) + 2,
+                           fuel((n - 1) / 2) + 2)
+        else:
+            table[n] = fuel(n / 2) + 1
+        return table[n]
+    return fuel(n)
+print(solution(50))
